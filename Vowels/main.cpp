129a8ae5e9c68e291d545112923e60a9aa2ff54b
Mariama Jaiteh <mjaiteh@smith.edu>
2016-10-18 14:29:45 -0400

    Int

--- Vowels/main.cpp
+++ Vowels/main.cpp
@@ -1,15 +1,31 @@
 //
 //  main.cpp
-//  Vowels
+//  Reverse  an Integer!
 //
 //  Created by Mariama Jaiteh on 10/7/16.
 //  Copyright © 2016 Mariama Jaiteh. All rights reserved.
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+int main () {
+    
+    int num;
+    int newNum = 0;
+    int remainder;
+    cout << "What is your number" << endl;
+    cin >> num;
+    
+    while(num != 0)
+    {
+        remainder = num % 10;
+        newNum = newNum*10 + remainder;
+        num /= 10;
+    }
+    cout << newNum << endl;
+    
+
+return 0;
 }
